07bd970ef51f3e09e7f5b3895633ba853ae1b48c
amh <alii_mohamed22@hotmail.com>
2023-06-14 11:05:01 +0300

    added a search bar and made the first letter capital

--- routes/recipe.js
+++ routes/recipe.js
@@ -35,12 +35,15 @@ const isLoggedIn = require('../lib/isLoggedIn')
 //controller_Call_router 
 router.get('/recipe/add',recipeController.recipe_add_get)
 router.post('/recipe/add', upload.single('uploadedFile'), recipeController.recipe_add_post)
+
 router.get('/recipe/index',recipeController.recipe_index_get)
+router.post('/recipe/index',recipeController.recipe_index_post)
 
 router.get('/recipe/edit', recipeController.recipe_edit_get)
 router.post('/recipe/edit', recipeController.recipe_edit_post)
 
 router.get('/recipe/detail',recipeController.recipe_detail_get)
+// router.post('/recipe/detail',recipeController.recipe_detail_post)
 // router.post('/recipe/detail',recipeController.recipe_detail_post)
 
 // router.post('/recipe/index',recipeController.recipe_delete_post)
